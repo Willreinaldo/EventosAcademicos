3b4496cd8b1ac8222683c8425151e0b4111fa037
Willian-Reinaldo <willian.reinaldo.m@gmail.com>
2023-04-19 15:01:49 -0300

    feat: add form of project

---
+++ index.js
@@ -0,0 +1,14 @@
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = process.env.API_PORT;
+
+app.listen(port, ()=>{
+    console.log(`App running on port ${port}.`);
+});
