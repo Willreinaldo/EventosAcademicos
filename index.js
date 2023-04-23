a793be11a6b3f4ef3ab3a7349bc6ea2ec3bf613f
Willian-Reinaldo <willian.reinaldo.m@gmail.com>
2023-04-23 17:30:25 -0300

    add docker-compose

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
-const port = process.env.API_PORT;
+const port = process.env.API_PORT || 8080;
 
 app.listen(port, ()=>{
     console.log(`App running on port ${port}.`);
